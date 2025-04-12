c02d57fa99e057a2f51bc86d99bb81a8d5db3cf2
d.klimkin <d.klimkin@edu.centraluniversity.ru>
2025-04-12 11:41:21 +0300

    change router ordering in main

--- timetable_bot/src/main.py
+++ timetable_bot/src/main.py
@@ -13,8 +13,8 @@ async def main() -> None:
     bot = Bot(TOKEN)
     dp = Dispatcher()
 
-    dp.include_router(command_handlers.router)
     dp.include_router(registration_handlers.router)
+    dp.include_router(command_handlers.router)
 
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
